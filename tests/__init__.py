f2b114fdf5cd2d56e9b4e00e3a32b48bdf6665b6
Ambuj Shukla <146608389+imambujshukla7@users.noreply.github.com>
2024-08-05 00:44:21 +0530

    Create __init__.py

---
+++ tests/__init__.py
@@ -0,0 +1,15 @@
+# Initializing the tests module
+
+from .test_data_loading import TestDataLoading
+from .test_model import TestModel
+from .test_training import TestTraining
+from .test_evaluation import TestEvaluation
+from .test_utils import TestUtils
+
+__all__ = [
+    'TestDataLoading',
+    'TestModel',
+    'TestTraining',
+    'TestEvaluation',
+    'TestUtils'
+]
